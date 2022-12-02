b6178fad997b33db4dd0f481b1bfadc6eb722a3c
ArtemyevArtyom <gof.ru@yandex.ru>
2022-12-02 18:41:24 +0400

    Переменные

---
+++ Homework1. Переменные, циклы/1. Переменные.py
@@ -0,0 +1,14 @@
+a = input('Введите ваше имя: ')
+print('Привет,', a, '!')
+b = input('Введите вашу должность: ')
+print('Отлично,', b, '!')
+First_Variable = 2
+Second_Variable = First_Variable
+Third_Variable = First_Variable * Second_Variable
+print('Первое значение', Third_Variable)
+Fourth_Variable = int(input('Введите второе значение: '))
+Fifth_Variable = Fourth_Variable + Third_Variable
+print('Результат сложения первого и второго значения', Fifth_Variable)
+Sixth_Variable = int(input('Коэффициент: '))
+Seventh_Variable = Sixth_Variable * Fifth_Variable
+print('Конечное значение', Seventh_Variable)
